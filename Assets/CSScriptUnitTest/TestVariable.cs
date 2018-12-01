4ccca4db8ae9320e04a1db4bf99ea4ae2441f3db
Masaki Nomura <masaki@full-spectrum.io>
2018-12-01 10:51:10 -0800

    line column test

--- Assets/CSScriptUnitTest/TestVariable.cs
+++ Assets/CSScriptUnitTest/TestVariable.cs
@@ -35,7 +35,18 @@ namespace CSScript {
 		[Test]
 		public void LineTest () {
 			CSNode root = ParseScript ("var d = 3;\n var e = 4; var h = 5;");
-			root.Evaluate ();
+
+			//position of '3' in 'var d = 3;'
+			Assert.AreEqual(1, root._children[0]._children[0]._children[1]._line);
+			Assert.AreEqual(8, root._children[0]._children[0]._children[1]._column);
+
+			//position of 'e' in 'var e = 4;'
+			Assert.AreEqual(2, root._children[1]._children[0]._children[0]._line);
+			Assert.AreEqual(1, root._children[1]._children[0]._children[0]._column);
+
+			//position of 'h' in 'var h = 5;'
+			Assert.AreEqual(2, root._children[2]._children[0]._children[0]._line);
+			Assert.AreEqual(12, root._children[2]._children[0]._children[0]._column);
 		}
 	}
 }
